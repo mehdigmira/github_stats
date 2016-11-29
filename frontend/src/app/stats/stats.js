20c9b3cd8a7d3a013a1a11afe7aed81727bdacb3
mgmira <mgmira@wiremind.fr>
2016-11-29 11:55:54 +0100

    into tabs

--- frontend/src/app/stats/stats.js
+++ frontend/src/app/stats/stats.js
@@ -6,11 +6,21 @@
     .controller('StatsController', StatsController);
 
   /** @ngInject */
-  function StatsController($http, $routeParams, $location, repoContributors, lastCommits) {
+  function StatsController($location, repoContributors, lastCommits) {
     var vm = this;
+
+    vm.tab = 1;
 
     vm.contributors = repoContributors;
     vm.commitsByUser = _.countBy(lastCommits, "login");
     vm.commitsByDate = _.countBy(lastCommits, function (o) { return moment(o['date']).format('YYYY-MM-DD'); })
+
+    vm.goHome = goHome;
+
+    ///////////
+
+    function goHome() {
+      $location.path('/').search({});
+    }
   }
 })();
